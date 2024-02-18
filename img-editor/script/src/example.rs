732140a68637f896ef072717bf1b6eec7dd912f0
Sofianel5 <sofianef1110@gmail.com>
2024-02-17 22:15:37 -0800

    server

---
+++ img-editor/script/src/example.rs
@@ -0,0 +1,65 @@
+use hex_literal::hex;
+use image::{DynamicImage, GenericImageView, ImageFormat, RgbImage};
+use lib::Transformation;
+use sp1_core::utils;
+use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use std::fs::File;
+
+fn main() {
+    utils::setup_logger();
+    // Generate proof.
+    let mut stdin = SP1Stdin::new();
+
+    // transformation
+    let data = include_str!("./transformations.json");
+    let decoded_transformations: Vec<Transformation> = serde_json::from_str(data).unwrap();
+
+    // image
+    let img = image::open("./src/256.png").unwrap();
+    let (width, height) = img.dimensions();
+    let img_buffer = img.into_bytes();
+
+    // signature (TODO: replace dummy values)
+    // reference: https://docs.rs/p256/latest/p256/ecdsa/index.html
+    let signing_key = hex!("ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf");
+    let ecdsa_signature: Vec<u8> = hex!("46557EFE96D22D07E104D9D7FAB558FB02F6B13116056E6D7C300D7BB132059907D538EAC68EC7864AA2AC2E23EA7082A04002B0ACDAC2FF8CCAD7E80E64DD00").to_vec();
+
+    // Write data.
+    stdin.write(&decoded_transformations);
+    stdin.write(&img_buffer);
+    stdin.write(&width);
+    stdin.write(&height);
+
+    stdin.write(&signing_key);
+    stdin.write(&ecdsa_signature);
+
+    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+
+    // Read transformed image.
+    let transformed_img_buf = proof.stdout.read::<Vec<u8>>();
+    let new_width = proof.stdout.read::<u32>();
+    let new_height = proof.stdout.read::<u32>();
+
+    println!(
+        "transformed_img_bug.len() = {:?}",
+        transformed_img_buf.len()
+    );
+    println!("new_width = {:?}", new_width);
+    println!("new_height = {:?}", new_height);
+
+    // Save transformed image.
+    let new_img = RgbImage::from_raw(new_width, new_height, transformed_img_buf).unwrap();
+    let fout = &mut File::create("./src/edited_img.jpg").unwrap();
+    new_img.write_to(fout, ImageFormat::Jpeg).unwrap();
+
+    // Verify proof.
+    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+
+    // Save proof.
+    proof
+        .save("proof-with-io.json")
+        .expect("saving proof failed");
+
+    println!("succesfully generated and verified proof for the program!");
+    
+}
--- img-editor/script/src/main.rs
+++ img-editor/script/src/main.rs
@@ -1,93 +1,88 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use hex_literal::hex;
-use image::{GenericImageView, ImageFormat, RgbImage};
-use lib::Transformation;
-use sp1_core::utils;
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
 use std::fs::File;
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
 
+use hex;
+use image::{DynamicImage, GenericImageView, RgbImage};
+use lib::Transformation;
+use sp1_core::utils::BabyBearBlake3;
+use sp1_core::SP1ProofWithIO;
+use sp1_core::{SP1Prover, SP1Stdin};
+use rocket::form::Form;
+use rocket::fs::{FileServer, TempFile};
+use sha2::{Sha256, Digest};
+
+
+#[macro_use] extern crate rocket;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("Request: {:#?}", http_request);
-}
-
-fn main() {
-    utils::setup_logger();
-    // Generate proof.
+fn generate_proof(tranformations: Vec<Transformation>, img: DynamicImage, public_key: Vec<u8>, ecdsa_signature: Vec<u8>) -> SP1ProofWithIO<BabyBearBlake3> {
     let mut stdin = SP1Stdin::new();
-
-    // transformation
-    let data = include_str!("./transformations.json");
-    let decoded_transformations: Vec<Transformation> = serde_json::from_str(data).unwrap();
-
-    // image
-    let img = image::open("./src/256.png").unwrap();
     let (width, height) = img.dimensions();
     let img_buffer = img.into_bytes();
 
-    // signature (TODO: replace dummy values)
-    // reference: https://docs.rs/p256/latest/p256/ecdsa/index.html
-    let signing_key = hex!("ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf");
-    let ecdsa_signature: Vec<u8> = hex!("46557EFE96D22D07E104D9D7FAB558FB02F6B13116056E6D7C300D7BB132059907D538EAC68EC7864AA2AC2E23EA7082A04002B0ACDAC2FF8CCAD7E80E64DD00").to_vec();
-
-    // Write data.
-    stdin.write(&decoded_transformations);
+    stdin.write(&tranformations);
     stdin.write(&img_buffer);
     stdin.write(&width);
     stdin.write(&height);
-
-    stdin.write(&signing_key);
+    stdin.write(&public_key);
     stdin.write(&ecdsa_signature);
 
-    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    return SP1Prover::prove(ELF, stdin).expect("proving failed");
+}
 
-    // Read transformed image.
+fn package_proof(mut proof: SP1ProofWithIO<BabyBearBlake3>, index: String) {
     let transformed_img_buf = proof.stdout.read::<Vec<u8>>();
     let new_width = proof.stdout.read::<u32>();
     let new_height = proof.stdout.read::<u32>();
 
-    println!(
-        "transformed_img_bug.len() = {:?}",
-        transformed_img_buf.len()
-    );
-    println!("new_width = {:?}", new_width);
-    println!("new_height = {:?}", new_height);
-
-    // Save transformed image.
     let new_img = RgbImage::from_raw(new_width, new_height, transformed_img_buf).unwrap();
-    let fout = &mut File::create("./src/edited_img.jpg").unwrap();
-    new_img.write_to(fout, ImageFormat::Jpeg).unwrap();
 
-    // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
-
-    // Save proof.
     proof
-        .save("proof-with-io.json")
+        .save(format!("./proofs/{index}/proof.json").as_str())
         .expect("saving proof failed");
 
-    println!("succesfully generated and verified proof for the program!");
+    let fout = &mut File::create(format!("./proofs/{index}/edited_img.jpg").as_str()).unwrap();
 
-    println!("Now running server to listen for incoming connections...");
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    new_img.write_to(fout, image::ImageFormat::Jpeg).unwrap();
+}
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+#[derive(FromForm)]
+struct ProofRequest<'r> {
+    transformations: String,
+    img_buffer: TempFile<'r>,
+    public_key: Vec<u8>,
+    ecdsa_signature: Vec<u8>,
+}
 
-        handle_connection(stream);
-    }
+#[post("/", data = "<proof_request>")]
+fn prove(proof_request: Form<ProofRequest<'_>>) -> String {
+    let transformations: Vec<Transformation> = serde_json::from_str(&proof_request.transformations).unwrap();
+    let img = image::open(proof_request.img_buffer.path().unwrap()).unwrap();
+    let public_key = &proof_request.public_key;
+    let ecdsa_signature = &proof_request.ecdsa_signature;
+
+    let proof = generate_proof(transformations, img, public_key.to_owned(), ecdsa_signature.to_owned());
+
+    let mut hasher = Sha256::new();
+
+    hasher.update(std::fs::read(proof_request.img_buffer.path().unwrap()).unwrap());
+    let result = hasher.finalize().to_vec();
+
+    package_proof(proof, hex::encode(result.clone()));
+
+    return hex::encode(result);
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .configure(rocket::Config::figment().merge(("port", 9999)))
+        .mount("/", routes![prove])
+        .mount("/proofs", FileServer::from("proofs/"))
+        .launch()
+        .await?;
+
+    Ok(())
 }
